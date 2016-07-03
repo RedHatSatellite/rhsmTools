a881812eb7717697d2e0ce50584f1d6e6b261a2e
Zaina Afoulki <zenetcontact@gmail.com>
2016-07-03 18:56:55 -0400

    Add --force flag and API call to unregister systems

--- rhDeleteStaleSubs.py
+++ rhDeleteStaleSubs.py
@@ -2,9 +2,9 @@
 
 # File: rhDeleteStaleSubs.py
 # Author: Zaina Afoulki <zaina@redhat.com>
-# Purpose: Given a username, password and date, delete subscription
-#                   for systems that didn't check in with RHSM  
-#                   since the provided date.
+# Purpose: Given a username, password and date, delete subscriptions
+#                   for systems that didn't check in with RHSM since
+#                   the provided date.
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -16,6 +16,7 @@
 # GNU General Public License for more details.
 
 import json
+import httplib
 import getpass
 import urllib2
 import base64
@@ -28,8 +29,8 @@ from datetime import datetime
 parser = OptionParser()
 parser.add_option("-l", "--login", dest="login", help="Login user for RHSM", metavar="LOGIN")
 parser.add_option("-p", "--password", dest="password", help="Password for specified user. Will prompt if omitted", metavar="PASSWORD")
-parser.add_option("-d", "--debug", dest='debug',help="print more details for debugging" , default=False, action='store_true')
-parser.add_option("-c", "--last-checkin", dest="checkin", help="Last Check in Date in the format yyyy-mm-dd", metavar="CHECKIN")
+parser.add_option("-c", "--last-checkin", dest="checkin", help="Last Date a System checked in with RHSM, format yyyy-mm-dd", metavar="CHECK-IN")
+parser.add_option("-f", "--force", dest="force", help="Do not ask prompt for confirmation, unregister all systems that match the last-checkin criteria", default=False, action='store_true')
 (options, args) = parser.parse_args()
 
 if not ( options.login ):
@@ -41,14 +42,13 @@ else:
     login = options.login
     password = options.password
     checkin = options.checkin
-
-yes = set(['yes','y', 'ye', ''])
-no = set(['no','n'])
+    force = options.force
 
 try:
     dt_reference = datetime.strptime(checkin, '%Y-%m-%d')
 except ValueError:
-    print "Incorrect format"
+    print "Incorrect Date format. Please use %Y-%m-%d. Example usage: ./rhDeleteStaleSubs.py -l rh_user_account -c 2016-02-10"
+    sys.exit(1)
 
 if not password: password = getpass.getpass("%s's password:" % login)
 
@@ -60,8 +60,6 @@ portal_host = "subscription.rhn.redhat.com"
 url = "https://" + portal_host + "/subscription/users/" + login + "/owners/"
 try:
  	request = urllib2.Request(url)
-        if options.debug :
-            print "Attempting to connect: " + url
 	base64string = base64.encodestring('%s:%s' % (login, password)).strip()
 	request.add_header("Authorization", "Basic %s" % base64string)
 	result = urllib2.urlopen(request)
@@ -79,9 +77,6 @@ for accounts in accountdata:
 
 #### Grab a list of Consumers
 url = "https://" + portal_host + "/subscription/owners/" + acct + "/consumers/"
-if options.debug :
-     print "Attempting to connect: " + url
-
 try:
  	request = urllib2.Request(url)
 	base64string = base64.encodestring('%s:%s' % (login, password)).strip()
@@ -103,8 +98,6 @@ for consumer in consumerdata:
 	consumerType = consumer["type"]["label"]
 	lastCheckin = consumer["lastCheckin"]
 	factsurl = "https://" + portal_host + "/subscription" + consumer["href"] + "/"
-        if options.debug :
-            print "Attempting to connect: " + factsurl
 	try:
 		sysinfo = urllib2.Request(factsurl)
 		base64string = base64.encodestring('%s:%s' % (login, password)).strip()
@@ -133,14 +126,21 @@ for consumer in consumerdata:
 		dt = parse_date(lastCheckin)
 		dt_no_tzinfo = dt.replace(tzinfo=None)
 		if dt_no_tzinfo <= dt_reference:
-			print "%s IP %s last checked-in %s which is older than %s. Delete? y/n" % (consumer["name"],ipaddr,dt_no_tzinfo,dt_reference)
-			choice = raw_input().lower()
-			if choice in yes:
-				# Delete System's Subscription from RHSM
+			print "Hostname: %s IP:%s UUID:%s last checked-in %s which is older than %s" % (consumer["name"],ipaddr,consumer["uuid"],dt_no_tzinfo.strftime("%Y-%m-%d"),dt_reference.strftime("%Y-%m-%d"))
+			choice = None
+			if not options.force: 
+				choice = raw_input("Unregister? y/n ").lower()
+			if choice in ['yes','y', 'YES', 'Y'] or options.force:
 				print "Removing System's Subscription from RHSM..."
-			elif choice in no:
-				# Skip this System
-				pass
+				try:
+					request = urllib2.Request(factsurl)
+					base64string = base64.encodestring('%s:%s' % (login, password)).strip()
+					request.add_header("Authorization", "Basic %s" % base64string)
+					request.get_method = lambda: 'DELETE'
+					result = urllib2.urlopen(request)
+				except Exception, e:
+					print "FATAL Error - %s" % (e)
+			
 			else:
-			   sys.stdout.write("Please respond with 'yes' or 'no'")
+				pass
 sys.exit(0)
